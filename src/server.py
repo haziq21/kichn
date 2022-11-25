290e29dac8db3cd0e94895fde353ee501040e0c7
Haziq Hairil <haziqhairil21@gmail.com>
2022-11-25 10:24:15 +0800

    Scaffold HTTP paths on server

--- src/server.py
+++ src/server.py
@@ -4,4 +4,25 @@ with the application running on the client.
 """
 
 from fastapi import FastAPI, WebSocket
+from pydantic import BaseModel
 from utils import database
+
+app = FastAPI()
+
+
+class LoginDetails(BaseModel):
+    pass
+
+
+class SignupDetails(LoginDetails):
+    pass
+
+
+@app.post("/login")
+def login(login_details: LoginDetails):
+    pass
+
+
+@app.post("/signup")
+def signup(signup_details: SignupDetails):
+    pass
