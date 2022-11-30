be14881fecf635280eeda3494935fa2d39b1d22f
Haziq Hairil <haziqhairil21@gmail.com>
2022-11-30 13:46:46 +0800

    Mark Redis & PasswordHasher instances as private

--- src/utils/database.py
+++ src/utils/database.py
@@ -8,8 +8,8 @@ import argon2
 import string
 import random
 
-r = redis.Redis()
-ph = argon2.PasswordHasher()
+_r = redis.Redis()
+_ph = argon2.PasswordHasher()
 
 #### ACCESSING THE APPLICATION ####
 
@@ -19,13 +19,13 @@ def login_is_valid(email: str, password: str) -> bool:
     Returns `True` if the user exists in the database
     and the password matches. Returns `False` otherwise.
     """
-    hashed_pw = r.get(f"user:{email}:auth")
+    hashed_pw = _r.get(f"user:{email}:auth")
 
     if hashed_pw is None:
         return False
 
     try:
-        return ph.verify(hashed_pw, password)
+        return _ph.verify(hashed_pw, password)
     except argon2.exceptions.VerifyMismatchError:
         return False
 
@@ -36,11 +36,11 @@ def create_user(name: str, email: str, password: str) -> bool:
     already exists in the database, and `True` otherwise. A return value
     of `True` can be taken to mean that the operation was successful.
     """
-    if r.get(f"user:{email}:auth") is not None:
+    if _r.get(f"user:{email}:auth") is not None:
         return False
 
-    r.set(f"user:{email}:name", name)
-    r.set(f"user:{email}:auth", password)
+    _r.set(f"user:{email}:name", name)
+    _r.set(f"user:{email}:auth", password)
 
     return True
 
@@ -51,7 +51,7 @@ def create_session(email: str) -> str:
     """
     chars = string.ascii_letters + string.digits
     session_id = "".join(random.choices(chars, k=20))
-    r.hset("pending-sessions", session_id, email)
+    _r.hset("pending-sessions", session_id, email)
 
     return session_id
 
@@ -61,10 +61,10 @@ def begin_session(session_id: str) -> str:
     Removes the session ID from the database to prevent the same session from
     being accessed again. Returns the email of the user whose session it is.
     """
-    email_bytes = r.hget("pending-sessions", session_id)
+    email_bytes = _r.hget("pending-sessions", session_id)
     assert email_bytes is not None
 
     email = str(email_bytes)
-    r.hdel("pending-sessions", session_id)
+    _r.hdel("pending-sessions", session_id)
 
     return email
