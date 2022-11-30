d257eddfb26c1cbbb5298f424915a95d52213b01
Haziq Hairil <haziqhairil21@gmail.com>
2022-11-30 10:21:04 +0800

    Move from FastAPI to aiohttp

--- src/server.py
+++ src/server.py
@@ -3,26 +3,69 @@ This module contains the web server that communicates
 with the application running on the client.
 """
 
-from fastapi import FastAPI, WebSocket
-from pydantic import BaseModel
+from aiohttp import web
 from utils import database
 
-app = FastAPI()
+
+async def login(request: web.Request):
+    """
+    Responds with a session token if the login credentials are valid.
+
+    The following is an example of the expected request body.
+
+    ```json
+    {
+        "email": "john@gmail.com",
+        "password": "correct horse battery staple"
+    }
+    ```
+
+    - 400 Bad Request is returned if the request body does not include these fields.
+    - 401 Unauthorized is returned if the login credentials are invalid.
+    - 200 OK is returned otherwise, along with a session token.
+
+    ```json
+    {
+        "sessionToken": "R7qnHymktvYDnpfb7ExB"
+    }
+    ```
+    """
+    return web.Response()
 
 
-class LoginDetails(BaseModel):
-    pass
+async def signup(request: web.Request):
+    """
+    Creates a new user account in the database and responds with a session token.
+
+    The following is an example of the expected request body.
+
+    ```json
+    {
+        "name": "John Doe",
+        "email": "john@gmail.com",
+        "password": "correct horse battery staple"
+    }
+    ```
+
+    - 400 Bad Request is returned if the request body does not include these fields.
+    - 409 Conflict is returned if the email already exists in the user database.
+    - 200 OK is returned otherwise, along with a session token.
+
+    ```json
+    {
+        "sessionToken": "R7qnHymktvYDnpfb7ExB"
+    }
+    ```
+    """
+    return web.Response()
 
 
-class SignupDetails(BaseModel):
-    pass
+app = web.Application()
+app.add_routes(
+    [
+        web.post("/login", login),
+        web.post("/signup", signup),
+    ]
+)
 
-
-@app.post("/login")
-def login(login_details: LoginDetails):
-    pass
-
-
-@app.post("/signup")
-def signup(signup_details: SignupDetails):
-    pass
+web.run_app(app)
