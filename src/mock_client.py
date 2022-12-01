867df9464e346f24c1755fec9e9d54b5001d1b21
Haziq Hairil <haziqhairil21@gmail.com>
2022-12-01 21:27:37 +0800

    Demonstrate logging in on mock client

--- src/mock_client.py
+++ src/mock_client.py
@@ -2,4 +2,16 @@
 This module simulates a headless (no GUI) client, for testing purposes.
 """
 
-from utils.networker import Networker
+import asyncio
+from utils import networker
+
+
+def handle_login(event: networker.AuthenticationEvent):
+    print(event)
+
+
+net = networker.Networker("http://0.0.0.0:8080")
+net.on_login = handle_login
+net.request_login("john@gmail.com", "correct horse battery staple")
+
+asyncio.run(net.run())
