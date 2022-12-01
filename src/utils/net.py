b6eca50d90876ef259f449ece0ea9098d6d4e774
Haziq Hairil <haziqhairil21@gmail.com>
2022-12-01 22:52:54 +0800

    Add docstring to `Networker`

--- src/utils/net.py
+++ src/utils/net.py
@@ -19,6 +19,21 @@ class AuthenticationEvent:
 
 
 class Networker:
+    """
+    Handles HTTP and WebSocket communication with the web server,
+    providing an abstracted interface over raw network operations.
+
+    HTTP requests made through this class adopt a two-part model of operation:
+    request scheduling and response handling. For example, to log in, the
+    `request_login()` method can be called. This method simply *schedules* an
+    HTTP request to be sent; it doesn't actually send the request itself. It
+    is hence non-blocking. Once the HTTP response is received, it is parsed
+    into (in this case) an `AuthenticationEvent` object, which is then passed
+    to the `on_login()` method as an argument. This model of operation allows
+    the client-side GUI code to continue running the GUI event loop while
+    waiting for HTTP responses, which makes the application feel more responsive.
+    """
+
     def __init__(self, server_url: str):
         self._server_url = server_url
         self._queued_reqs: list[Callable[[aiohttp.ClientSession], Coroutine]] = []
@@ -79,11 +94,11 @@ class Networker:
             # Make the POST request to /login
             async with session.post("/login", json=req_body) as res:
                 if res.status == 200:
-                    res_body = await res.json()
-                    # The login was successful
-                    self.on_login(AuthenticationEvent(True, res_body["sessionToken"]))
+                    # 200 OK - the login was successful
+                    session_token = (await res.json())["sessionToken"]
+                    self.on_login(AuthenticationEvent(True, session_token))
                 elif res.status == 401:
-                    # The login was unsuccessful
+                    # 401 Unauthorized - the login was unsuccessful
                     self.on_login(AuthenticationEvent(False, ""))
                 else:
                     # Something went wrong...
@@ -102,11 +117,11 @@ class Networker:
             # Make the POST request to /signup
             async with session.post("/signup", json=req_body) as res:
                 if res.status == 200:
-                    res_body = await res.json()
-                    # The signup was successful
-                    self.on_login(AuthenticationEvent(True, res_body["sessionToken"]))
+                    # 200 OK - the signup was successful
+                    session_token = (await res.json())["sessionToken"]
+                    self.on_login(AuthenticationEvent(True, session_token))
                 elif res.status == 401:
-                    # The signup was unsuccessful
+                    # 401 Unauthorized - the signup was unsuccessful
                     self.on_login(AuthenticationEvent(False, ""))
                 else:
                     # Something went wrong...
