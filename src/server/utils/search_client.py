d3d990023369cd88143a5076af6f4009844a3b71
Haziq Hairil <haziqhairil21@gmail.com>
2022-12-21 19:53:32 +0900

    Scaffold search client

---
+++ src/server/utils/search_client.py
@@ -0,0 +1,13 @@
+"""
+This module exposes high-level functions to interface 
+with the Meilisearch full-text search engine.
+"""
+
+import meilisearch
+
+
+class SearchClient:
+    """Handles communication with the Meilisearch server."""
+
+    def __init__(self):
+        pass
