e2f0a1a99f31198caf300823b7de67f0e2c11392
Haziq Hairil <haziqhairil21@gmail.com>
2022-12-21 23:15:20 +0900

    Move DB funcs into DatabaseClient class

--- src/server/utils/database.py
+++ src/server/utils/database.py
@@ -10,10 +10,6 @@ import argon2
 import string
 import random
 
-# TODO: Move these into a DB class
-_r = redis.Redis()
-_ph = argon2.PasswordHasher()
-
 
 def _gen_random_id(k=20) -> str:
     """
@@ -23,118 +19,116 @@ def _gen_random_id(k=20) -> str:
     return "".join(random.choices(sample_chars, k=k))
 
 
-#### BUILDING THE DEFAULT DATABASE ####
+class DatabaseClient:
+    def __init__(self):
+        self._r = redis.Redis()
+        self._ph = argon2.PasswordHasher()
 
+    #### BUILDING THE DEFAULT DATABASE ####
 
-def create_default_product(name: str, category: str, barcodes: list[int]) -> str:
-    """
-    Creates a new default product in the database.
-    Returns the generated ID of the product.
-    """
-    product_id = _gen_random_id()
+    def create_default_product(
+        self, name: str, category: str, barcodes: list[int]
+    ) -> str:
+        """
+        Creates a new default product in the database.
+        Returns the generated ID of the product.
+        """
+        product_id = _gen_random_id()
 
-    # Write the product data to the database
-    _r.set(f"product:{product_id}:name", name)
-    _r.set(f"product:{product_id}:category", category)
-    _r.hset("barcodes", mapping={str(b): product_id for b in barcodes})
+        # Write the product data to the database
+        self._r.set(f"product:{product_id}:name", name)
+        self._r.set(f"product:{product_id}:category", category)
+        # TODO: What if the barcode already exists in the database?
+        self._r.hset("barcodes", mapping={str(b): product_id for b in barcodes})
 
-    return product_id
+        return product_id
 
+    #### ACCESSING THE APPLICATION ####
 
-#### ACCESSING THE APPLICATION ####
+    def login_is_valid(self, email: str, password: str) -> bool:
+        """
+        Returns `True` if the user exists in the database
+        and the password matches. Returns `False` otherwise.
+        """
+        hashed_pw = self._r.get(f"user:{email}:auth")
 
+        if hashed_pw is None:
+            # User does not exist
+            return False
 
-def login_is_valid(email: str, password: str) -> bool:
-    """
-    Returns `True` if the user exists in the database
-    and the password matches. Returns `False` otherwise.
-    """
-    hashed_pw = _r.get(f"user:{email}:auth")
+        try:
+            # User exists and password is correct
+            return self._ph.verify(hashed_pw, password)
+        except argon2.exceptions.VerifyMismatchError:
+            # Password is incorrect
+            return False
 
-    if hashed_pw is None:
-        # User does not exist
-        return False
+    def create_user(self, name: str, email: str, password: str) -> bool:
+        """
+        Creates a user in the database. Returns `False` if the user's email
+        already exists in the database, and `True` otherwise. A return value
+        of `True` can be taken to mean that the operation was successful.
+        """
+        if self._r.get(f"user:{email}:auth") is not None:
+            # User already exists
+            return False
 
-    try:
-        # User exists and password is correct
-        return _ph.verify(hashed_pw, password)
-    except argon2.exceptions.VerifyMismatchError:
-        # Password is incorrect
-        return False
+        # Write the user's account data to the database
+        self._r.set(f"user:{email}:name", name)
+        self._r.set(f"user:{email}:auth", self._ph.hash(password))
 
+        return True
 
-def create_user(name: str, email: str, password: str) -> bool:
-    """
-    Creates a user in the database. Returns `False` if the user's email
-    already exists in the database, and `True` otherwise. A return value
-    of `True` can be taken to mean that the operation was successful.
-    """
-    if _r.get(f"user:{email}:auth") is not None:
-        # User already exists
-        return False
+    def create_session(self, email: str) -> str:
+        """
+        Creates and returns a session token for the specified user.
+        """
+        session_token = _gen_random_id()
+        self._r.hset("pending-sessions", session_token, email)
 
-    # Write the user's account data to the database
-    _r.set(f"user:{email}:name", name)
-    _r.set(f"user:{email}:auth", _ph.hash(password))
+        return session_token
 
-    return True
+    def begin_session(self, session_token: str) -> str:
+        """
+        Removes the session token from the database to prevent the same session from
+        being accessed again. Returns the email of the user whose session it is.
+        Raises an AssertionError if the session token does not exist in the database.
+        """
+        email_bytes = self._r.hget("pending-sessions", session_token)
+        assert email_bytes is not None
 
+        # Delete the session from the database
+        self._r.hdel("pending-sessions", session_token)
 
-def create_session(email: str) -> str:
-    """
-    Creates and returns a session token for the specified user.
-    """
-    session_token = _gen_random_id()
-    _r.hset("pending-sessions", session_token, email)
+        return str(email_bytes)
 
-    return session_token
+    #### MODIFYING KITCHENS ####
 
+    def create_kitchen(self, email: str, kitchen_name: str) -> str:
+        """
+        Creates a new kitchen and assigns the indicated
+        user as the owner. Returns the kitchen's ID.
+        """
+        kitchen_id = _gen_random_id()
 
-def begin_session(session_token: str) -> str:
-    """
-    Removes the session token from the database to prevent the same session from
-    being accessed again. Returns the email of the user whose session it is.
-    Raises an AssertionError if the session token does not exist in the database.
-    """
-    email_bytes = _r.hget("pending-sessions", session_token)
-    assert email_bytes is not None
+        # Write the kitchen data to the database
+        self._r.set(f"kitchen:{kitchen_id}:name", kitchen_name)
+        self._r.sadd(f"user:{email}:owned-kitchens", kitchen_id)
 
-    # Delete the session from the database
-    _r.hdel("pending-sessions", session_token)
+        return kitchen_id
 
-    return str(email_bytes)
+    def rename_kitchen(self, kitchen_id: str, new_name: str):
+        """
+        Sets the name of the kitchen to `new_name`.
+        """
+        self._r.set(f"kitchen:{kitchen_id}:name", new_name)
 
-
-#### MODIFYING KITCHENS ####
-
-
-def create_kitchen(email: str, kitchen_name: str) -> str:
-    """
-    Creates a new kitchen and assigns the indicated
-    user as the owner. Returns the kitchen's ID.
-    """
-    kitchen_id = _gen_random_id()
-
-    # Write the kitchen data to the database
-    _r.set(f"kitchen:{kitchen_id}:name", kitchen_name)
-    _r.sadd(f"user:{email}:owned-kitchens", kitchen_id)
-
-    return kitchen_id
-
-
-def rename_kitchen(kitchen_id: str, new_name: str):
-    """
-    Sets the name of the kitchen to `new_name`.
-    """
-    _r.set(f"kitchen:{kitchen_id}:name", new_name)
-
-
-def delete_kitchen(kitchen_id: str):
-    """
-    Deletes the kitchen from the database.
-    """
-    # TODO: Remember to delete the kitchen ID from users' owned-kitchens and shared-kitchens
-    # Get all the Redis keys belonging to the specified kitchen
-    kitchen_keys = _r.keys(f"kitchen:{kitchen_id}:*")
-    # Delete the keys
-    _r.delete(*kitchen_keys)
+    def delete_kitchen(self, kitchen_id: str):
+        """
+        Deletes the kitchen from the database.
+        """
+        # TODO: Remember to delete the kitchen ID from users' owned-kitchens and shared-kitchens
+        # Get all the Redis keys belonging to the specified kitchen
+        kitchen_keys = self._r.keys(f"kitchen:{kitchen_id}:*")
+        # Delete the keys
+        self._r.delete(*kitchen_keys)
