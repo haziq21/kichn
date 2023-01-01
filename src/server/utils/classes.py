4b34d2130c37d375454f11f8ce4863af007fb72d
Haziq Hairil <haziqhairil21@gmail.com>
2023-01-01 18:23:32 +0800

    Scaffold classes.py

---
+++ src/server/utils/classes.py
@@ -0,0 +1,9 @@
+"""
+This module provides dataclasses to work with application-specific data.
+"""
+
+from dataclasses import dataclass
+
+# TODO: Kitchen
+# TODO: Product (with amount)
+# TODO: User
