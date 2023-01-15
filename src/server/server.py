c256222b6ec1adcc2548bb0f8822c67d262c281d
OohACactus <evan_choo_qi_jie@s2020.ssts.edu.sg>
2023-01-15 14:05:38 +0800

    implement post /signup and post /login

--- src/server/server.py
+++ src/server/server.py
@@ -8,6 +8,29 @@ from utils import database
 
 
 async def login(request: web.Request):
+    body = await request.post()
+    login = body["login"]
+    password = body["password"]
+
+    """
+    if "email" not in body or "password" not in body:
+        return web.json_response(status=400)
+        # Runs if the request body does not include these fields.
+    """
+
+    if not (database.login_is_valid(body["email"], body["password"])):
+        res = web.Response(status=401)
+        return res
+        # Runs if the login credentials are invalid.
+
+    else:
+        ses_create = database.create_session(body["email"])
+        res = web.Response(status=200)
+        res.set_cookie("session_Token", ses_create)
+        return res
+
+        # Runs if returned otherwise [code is good to go]
+
     """
     Responds with a session token if the login credentials are valid.
 
@@ -24,18 +47,37 @@ async def login(request: web.Request):
     - 401 Unauthorized is returned if the login credentials are invalid.
     - 200 OK is returned otherwise, along with a session token.
 
-    The following is an example body of a 200 OK response.
-
     ```json
     {
         "sessionToken": "R7qnHymktvYDnpfb7ExB"
     }
     ```
     """
-    return web.Response()
 
 
 async def signup(request: web.Request):
+    apply = await request.post()
+    login = apply["login"]
+    password = apply["password"]
+
+    """
+    if "name" not in apply or "email" not in apply or "password" not in apply:
+        return web.json_response(status=400)
+        # Runs if the request body does not include these fields.
+    """
+
+    if not database.create_user(apply["name"], apply["email"], apply["password"]):
+        res = web.Response(status=409)
+        return res
+        # Runs if the email already exists in the user database.
+
+    ses_create = database.create_session(apply["email"])
+    res = web.Response(status=200)
+    res.set_cookie("session_token", ses_create)
+    return res
+
+    # Runs if returned otherwise [code is good to go]
+
     """
     Creates a new user account in the database and responds with a session token.
 
@@ -53,15 +95,16 @@ async def signup(request: web.Request):
     - 409 Conflict is returned if the email already exists in the user database.
     - 200 OK is returned otherwise, along with a session token.
 
-    The following is an example body of a 200 OK response.
-
     ```json
     {
         "sessionToken": "R7qnHymktvYDnpfb7ExB"
     }
     ```
     """
-    return web.Response()
+
+
+async def kitchens_page(request: web.Request):
+    pass
 
 
 app = web.Application()
@@ -69,7 +112,11 @@ app.add_routes(
     [
         web.post("/login", login),
         web.post("/signup", signup),
+        web.get("/login", login),
+        web.post("/signup", signup),
+        web.get("/kitchens", kitchens_page),
     ]
 )
 
 web.run_app(app)
+
