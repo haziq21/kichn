f4b23c0f132f1ec4461b9760dcff3bd1b0584d91
OohACactus <evan_choo_qi_jie@s2020.ssts.edu.sg>
2023-01-18 16:45:37 +0800

    implement get /login and get signup

--- src/server/server.py
+++ src/server/server.py
@@ -4,33 +4,19 @@ with the application running on the client.
 """
 
 from aiohttp import web
-from utils import database
+from utils.database import DatabaseClient
+from utils.templating import Templator
+
+
+def html_response(body: str):
+    return web.Response(body=body, content_type="text/html")
+
+
+async def kitchens_page(request: web.Request):
+    return html_response("Hello, World!")
 
 
 async def login(request: web.Request):
-    body = await request.post()
-    login = body["login"]
-    password = body["password"]
-
-    """
-    if "email" not in body or "password" not in body:
-        return web.json_response(status=400)
-        # Runs if the request body does not include these fields.
-    """
-
-    if not (database.login_is_valid(body["email"], body["password"])):
-        res = web.Response(status=401)
-        return res
-        # Runs if the login credentials are invalid.
-
-    else:
-        ses_create = database.create_session(body["email"])
-        res = web.Response(status=200)
-        res.set_cookie("session_Token", ses_create)
-        return res
-
-        # Runs if returned otherwise [code is good to go]
-
     """
     Responds with a session token if the login credentials are valid.
 
@@ -54,30 +40,25 @@ async def login(request: web.Request):
     ```
     """
 
+    body = await request.post()
+    email = body["email"]
+    password = body["password"]
+
+    # if not db.login_is_valid(email, password):
+
+    if False:
+        # Runs if the login credentials are invalid.
+        return html_response(body=templator.login_failed())
+    else:
+        # Runs if returned otherwise [code is good to go]
+        ses_create = db.create_session(email)
+        res = web.Response(headers={"HX-redirect": "/"})
+        res.set_cookie("session_token", ses_create)
+
+        return res
+
 
 async def signup(request: web.Request):
-    apply = await request.post()
-    login = apply["login"]
-    password = apply["password"]
-
-    """
-    if "name" not in apply or "email" not in apply or "password" not in apply:
-        return web.json_response(status=400)
-        # Runs if the request body does not include these fields.
-    """
-
-    if not database.create_user(apply["name"], apply["email"], apply["password"]):
-        res = web.Response(status=409)
-        return res
-        # Runs if the email already exists in the user database.
-
-    ses_create = database.create_session(apply["email"])
-    res = web.Response(status=200)
-    res.set_cookie("session_token", ses_create)
-    return res
-
-    # Runs if returned otherwise [code is good to go]
-
     """
     Creates a new user account in the database and responds with a session token.
 
@@ -102,21 +83,60 @@ async def signup(request: web.Request):
     ```
     """
 
+    body = await request.post()
+    username = body["username"]
+    password = body["password"]
+    email = body["email"]
 
-async def kitchens_page(request: web.Request):
-    pass
+    if not db.create_user(username, email, password):
+        return html_response(body=templator.signup_failed())
+        # Runs if the email already exists in the user database.
+
+    ses_create = db.create_session(email)
+    res = web.Response(status=200)
+    res.set_cookie("session_token", ses_create)
+    return res
+
+    # Runs if returned otherwise [code is good to go]
 
 
+async def signup_page(request: web.Request):
+    return html_response(templator.signup())
+
+
+async def login_page(request: web.Request):
+    return html_response(templator.login())
+
+
+async def static_asset(request: web.Request):
+    filepath = request.match_info["filepath"]
+    file = db.get_static_asset(filepath)
+
+    if file is None:
+        return web.Response(status=404)
+
+    content_types = {
+        "css": "text/css",
+        "js": "text/javascript",
+        "svg": "image/svg+xml",
+    }
+
+    file_ext = filepath.split(".")[-1]
+    return web.Response(body=file, content_type=content_types[file_ext])
+
+
+db = DatabaseClient("src/client/static", "server-store")
+templator = Templator("src/client/templates")
 app = web.Application()
 app.add_routes(
     [
+        web.get("/", kitchens_page),
+        web.post("/signup", signup),
         web.post("/login", login),
-        web.post("/signup", signup),
-        web.get("/login", login),
-        web.post("/signup", signup),
-        web.get("/kitchens", kitchens_page),
+        web.get("/signup", signup_page),
+        web.get("/login", login_page),
+        web.get("/static/{filepath}", static_asset),
     ]
 )
 
 web.run_app(app)
-
