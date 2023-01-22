749f0b0c369672c3c37cfe597dd1729eab09a1c1
Lohithsst <tanuku_sai_surya_lohith@s2020.ssts.edu.sg>
2023-01-22 21:09:40 +0800

    Update search.py

--- src/server/utils/search.py
+++ src/server/utils/search.py
@@ -12,22 +12,21 @@ class SearchClient:
     def __init__(self):
         self._client = meilisearch.Client("http://localhost:7700")
 
-    def index_default_products(self, product_names: dict[str, str]):
+    def _add_products_to_index(self, index_name: str, product_names: dict[str, str]):
         documents = []
-
         for product_id in product_names:
             doc = {
                 "id": product_id,
                 "name": product_names[product_id],
             }
             documents.append(doc)
-
         if documents:
-            self._client.index("default").add_documents(documents)
+            self._client.index(index_name).add_documents(documents)
+
+    def index_default_products(self, product_names: dict[str, str]):
+        self._add_products_to_index("default", product_names)
 
     def index_inventory_products(self, kitchen_id: str, product_names: dict[str, str]):
-
-        self._client.index(kitchen_id + "-inventory").add_documents(documents)
         """
         Indexes the given products as items of the kitchen's inventory.
         `product_names` maps product IDs to product names.
@@ -43,6 +42,8 @@ class SearchClient:
         # Add these documents to the `kID-inventory` index, where
         # `kID` is `kitchen_id`. E.g., if `kitchen_id` were "abc123",
         # the meilisearch index uid would be "abc123-inventory".
+
+        self._add_products_to_index(kitchen_id + "-inventory", product_names)
 
     def index_grocery_products(self, kitchen_id: str, product_names: dict[str, str]):
         """
@@ -61,22 +62,27 @@ class SearchClient:
         # `kID` is `kitchen_id`. E.g., if `kitchen_id` were "abc123",
         # the meilisearch index uid would be "abc123-grocery".
 
+        self._add_products_to_index(kitchen_id + "-grocery", product_names)
+
     def search_default_products(self, query: str) -> list[str]:
         """
         Returns the IDs of default products that match the search query.
         """
-        return []
+        search_result = self._client.index("default").search(query)
+        return [i["id"] for i in search_result["hits"]]
 
     def search_inventory_products(self, kitchen_id: str, query: str) -> list[str]:
         """
         Returns the IDs of products that are in the specified
         inventory list which match the search query.
         """
-        return []
+        search_result = self._client.index(kitchen_id + "-inventory").search(query)
+        return [i["id"] for i in search_result["hits"]]
 
     def search_grocery_products(self, kitchen_id: str, query: str) -> list[str]:
         """
         Returns the IDs of products that are in the specified
         grocery list which match the search query.
         """
-        return []
+        search_result = self._client.index(kitchen_id + "-grocery").search(query)
+        return [i["id"] for i in search_result["hits"]]
