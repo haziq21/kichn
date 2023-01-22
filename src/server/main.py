df394cca630b406b988d32cb480fa3a4e3479b6d
Haziq Hairil <haziqhairil21@gmail.com>
2023-01-22 18:11:22 +0800

    Re-raise web server exception

--- src/server/main.py
+++ src/server/main.py
@@ -45,10 +45,11 @@ def main():
     try:
         # Run the web server
         import server
-    except Exception:
+    except Exception as e:
         # Terminate the Redis and Meilisearch servers when the web server is killed
         redis_proc.terminate()
         meilisearch_proc.terminate()
+        raise e
 
 
 main()
