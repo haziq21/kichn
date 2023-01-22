797f53a04f8e8843a441062dcc15588edbd8a4af
Haziq Hairil <haziqhairil21@gmail.com>
2023-01-22 21:36:48 +0800

    Complete `search.py` scaffold

--- src/server/utils/search.py
+++ src/server/utils/search.py
@@ -12,6 +12,8 @@ class SearchClient:
     def __init__(self):
         self._client = meilisearch.Client("http://localhost:7700")
 
+    #### HELPER FUNCTIONS ####
+
     def _add_products_to_index(self, index_name: str, product_names: dict[str, str]):
         """
         Adds the given products to the specified search index.
@@ -28,6 +30,16 @@ class SearchClient:
 
         if documents:
             self._client.index(index_name).add_documents(documents)
+
+    def _search_index(self, index_name: str, query: str) -> list[str]:
+        """
+        Returns the IDs of products from the
+        specified index that match the search query.
+        """
+        # TODO: This.
+        return []
+
+    #### PRODUCT INDEXING ####
 
     def index_default_products(self, product_names: dict[str, str]):
         """
@@ -50,6 +62,15 @@ class SearchClient:
         """
         self._add_products_to_index(kitchen_id + "-grocery", product_names)
 
+    def index_custom_products(self, kitchen_id: str, product_names: dict[str, str]):
+        """
+        Indexes the given products as custom products of the kitchen.
+        `product_names` maps product IDs to product names.
+        """
+        # TODO: This.
+
+    #### PRODUCT SEARCHING ####
+
     def search_default_products(self, query: str) -> list[str]:
         """Returns the IDs of default products that match the search query."""
         search_result = self._client.index("default").search(query)
@@ -70,3 +91,22 @@ class SearchClient:
         """
         search_result = self._client.index(kitchen_id + "-grocery").search(query)
         return [i["id"] for i in search_result["hits"]]
+
+    def search_custom_products(self, kitchen_id: str, query: str) -> list[str]:
+        """
+        Returns the IDs of custom products from the
+        specified kitchen which match the search query.
+        """
+        # TODO: This.
+        return []
+
+    #### PRODUCT DELETION ####
+
+    def delete_inventory_product(self, kitchen_id: str, product_id: str):
+        """Removes the specified product from the kitchen's inventory index."""
+
+    def delete_grocery_product(self, kitchen_id: str, product_id: str):
+        """Removes the specified product from the kitchen's grocery index."""
+
+    def delete_custom_product(self, kitchen_id: str, product_id: str):
+        """Removes the specified product from the kitchen's custom product index."""
