aaa03a43a371b3a5005c16fb6daf8206413e2364
OohACactus <evan_choo_qi_jie@s2020.ssts.edu.sg>
2023-01-31 22:05:28 +0800

    product_image(), changed non-200 responses to HTTP

--- src/server/server.py
+++ src/server/server.py
@@ -173,6 +173,30 @@ async def grocery_page(request: web.Request):
     return html_response(templator.grocery_page(page_data))
 
 
+async def product_image(request: web.Request):
+    email = extract_request_owner(request)
+
+    if email is None:
+        # The user isn't signed in
+        raise web.HTTPUnauthorized()
+
+    kitchen_id = request.match_info["kitchen_id"]
+    product_id = request.match_info["product_id"]
+    product_img = db.get_product_image(kitchen_id, product_id)
+    access = db.user_has_access_to_kitchen(email, kitchen_id)
+
+    if not access:
+        # User has no access to kitchen
+        raise web.HTTPForbidden()
+
+    if product_img is None:
+        # If image does not exist
+        raise web.HTTPNotFound()
+
+    # Returns image in jpeg form.
+    return web.Response(body=product_img, content_type="image/jpeg")
+
+
 #### MISC ####
 
 
