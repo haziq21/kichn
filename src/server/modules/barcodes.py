445f1624ac956f3ac85977a2e5a57d7cde288d1c
Lohithsst <tanuku_sai_surya_lohith@s2020.ssts.edu.sg>
2023-02-11 22:01:12 +0800

    Finished barcodes.py

--- src/server/modules/barcodes.py
+++ src/server/modules/barcodes.py
@@ -1,11 +1,32 @@
-""""Handles barcodes."""
+""""
+Handles barcodes.
+
+Authored by Lohith Tanuku.
+"""
 
 from pyzbar import pyzbar
+from pyzbar.pyzbar import ZBarSymbol
 from typing import Optional
+from PIL import Image
+import io
 
 
-def read_barcodes(image: bytes) -> Optional[int]:
+def read_barcodes(raw_image: bytes) -> Optional[int]:
     """
     Reads a barcode from the supplied image. Returns the first
     EAN-13 barcode found, or `None` if no barcodes were found.
     """
+    # Create an Image object from the input bytes
+    image = Image.open(io.BytesIO(raw_image))
+
+    # Convert the Image object to grayscale
+    image = image.convert("L")
+
+    # Find the barcodes in the image and decode each of the barcodes
+    barcodes = pyzbar.decode(image, symbols=[ZBarSymbol.EAN13])
+
+    if not barcodes:
+        return None
+
+    # Converts barcode data from a bytes object to an integer
+    return int(barcodes[0])
