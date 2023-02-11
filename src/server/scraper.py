06d4ae99d3bb7977fa9746e499e685469d18f578
Lohithsst <tanuku_sai_surya_lohith@s2020.ssts.edu.sg>
2023-02-11 20:51:51 +0800

    hopefully finished scraper

--- src/server/scraper.py
+++ src/server/scraper.py
@@ -6,52 +6,45 @@ import asyncio
 import aiohttp
 from modules.database import DatabaseClient
 import json
-import requests
-
-
-db = DatabaseClient("src/client/static", "server-store")
 
 
 class Scraper:
-    def __init__(self, categories: dict[str, str]):
+    def __init__(
+        self,
+        categories: dict[str, str],
+        db,
+    ):
         # Maps category slugs to category names
         self.categories: dict[str, str] = categories
+        # sets the object attribute for db
+        self.db = db
 
-    async def extract_product(
-        self, session: aiohttp.ClientSession, product: dict, category_slug
+    async def scrape(
+        self,
+        session: aiohttp.ClientSession,
     ):
-        """
-        Given a raw product dictionary supplied by the FairPrice API,
-        extracts the necessary information and writes it to the database.
-        """
+        """Scrapes all the products from the FairPrice API and writes the scraped data to the database."""
+        tasks = [self.scrape_category(session, cat) for cat in self.categories.keys()]
+        await asyncio.gather(*tasks)
 
-        # Get product name
-        name = product["name"]
-        barcodes = []
-        image = b""
+    async def scrape_category(
+        self,
+        session: aiohttp.ClientSession,
+        category: str,
+    ):
+        """Scrapes data from one category of the API and writes it to the database."""
 
-        # Get product category
-        category = categories[category_slug]
+        total_pages = await self.scrape_page(session, category, 1)
 
-        # Get product barcode
-        try:
-            barcodes = [int(bar) for bar in product["barcodes"]]
-        except TypeError:
-            print(product["name"], "no barcode")
-
-        # Get product image
-        try:
-            image_url = product["images"][0]
-            async with session.get(image_url) as res:
-                image = await res.read()
-        except TypeError:
-            print(product["name"], "no image")
-
-        # Adds product to database
-        product_id = db.create_default_product(name, category, barcodes, image)
+        # Scrape all the other pages in parallel
+        tasks = [self.scrape_page(session, category, i) for i in range(total_pages)]
+        await asyncio.gather(*tasks)
 
     async def scrape_page(
-        self, session: aiohttp.ClientSession, category_slug: str, page: int
+        self,
+        session: aiohttp.ClientSession,
+        category_slug: str,
+        page: int,
     ) -> int:
 
         """
@@ -67,18 +60,22 @@ class Scraper:
 
         # Gets current page to show progress
         curr_page = 0
-
+        curr_page = body["data"]["pagination"]["page"]
+        """
         try:
             curr_page = body["data"]["pagination"]["page"]
         except:
-            print("curr_page", category_slug, "/", page)
+            print("curr_page", category_slug, "/", page)"""
 
         # Get total pages in the category
         total_pages = 0
+        total_pages = body["data"]["pagination"]["total_pages"]
+        """
         try:
             total_pages = body["data"]["pagination"]["total_pages"]
         except:
             print("total_page", category_slug, "/", page)
+        """
 
         # Print current progress
         print(f"Scraped page {curr_page} / {total_pages} of category {category_slug}")
@@ -92,24 +89,49 @@ class Scraper:
             print("no product", category_slug, "/", page)
         await asyncio.gather(*tasks)
 
-        # return total number of pages
         return total_pages
 
-    async def scrape_category(self, session: aiohttp.ClientSession, category: str):
-        """Scrapes data from one category of the API and writes it to the database."""
+    async def extract_product(
+        self,
+        session: aiohttp.ClientSession,
+        product: dict,
+        category_slug,
+    ):
+        """
+        Given a raw product dictionary supplied by the FairPrice API,
+        extracts the necessary information and writes it to the database.
+        """
 
-        total_pages = await self.scrape_page(session, category, 1)
+        # Get product name
+        name = product["name"]
+        barcodes = []
+        image = b""
 
-        # Scrape all the other pages in parallel
-        tasks = [self.scrape_page(session, category, i) for i in range(total_pages)]
-        await asyncio.gather(*tasks)
+        # Get product category
+        category = self.categories[category_slug]
 
-    async def scrape(self, session: aiohttp.ClientSession):
-        """Scrapes all the products from the FairPrice API and writes the scraped data to the database."""
-        tasks = [self.scrape_category(session, cat) for cat in self.categories.keys()]
-        await asyncio.gather(*tasks)
+        # Get product barcode
+        if product["barcodes"] is None:
+            barcodes = []
+        else:
+            barcodes = [int(bar) for bar in product["barcodes"]]
 
-    def api_url(self, product_category: str, page_number=1) -> str:
+        # Get product image
+        if product["images"] is None:
+            image = None
+        else:
+            image_url = product["images"][0]
+            async with session.get(image_url) as res:
+                image = await res.read()
+
+        # Adds product to database
+        product_id = self.db.create_default_product(name, category, barcodes, image)
+
+    def api_url(
+        self,
+        product_category: str,
+        page_number=1,
+    ) -> str:
         """Returns the URL of the FairPrice API endpoint."""
         return (
             "https://website-api.omni.fairprice.com.sg/api/product/v2"
@@ -121,33 +143,24 @@ async def main():
     """
     Runs the scraper
     """
-    # Prevents aiohttp from timing out
-    session_timout = aiohttp.ClientTimeout(total=None)
-    scraper = Scraper(categories)
-    async with aiohttp.ClientSession(timeout=session_timout) as session:
-        await scraper.scrape(session)
+    # Read FairPrice categories
+    with open("fairprice_categories.json") as cats:
+        fairprice_categories = json.load(cats)
 
-
-# Read FairPrice categories
-with open("fairprice_categories.json") as cats:
-    fairprice_categories = json.load(cats)
-
-
-def get_fairprice_slugs(fairprice_categories):
-    """
-    Gets the slugs from fairpricecategories.json to be used in the URL.
-    """
+    # Gets the slugs from fairpricecategories.json to be used in the URL and the Category names. Then, puts them into a dict
     categories = {}
     for cat in fairprice_categories:
         for sub_cat in cat["menu"]:
             slug = sub_cat["url"].split("/")[-1]
             category = sub_cat["name"]
             categories[slug] = category
-    return categories
 
+    # Prevents aiohttp from timing out
+    session_timout = aiohttp.ClientTimeout(total=None)
+    db = DatabaseClient("src/client/static", "server-store")
+    scraper = Scraper(categories, db)
+    async with aiohttp.ClientSession(timeout=session_timout) as session:
+        await scraper.scrape(session)
 
-# Makes a dict of all the slugs for URL and the category names
-categories = get_fairprice_slugs(fairprice_categories)
-print(type(categories))
 
 asyncio.run(main())
