c079fa329b3866ee820422efac57523b38abad4b
Lohithsst <tanuku_sai_surya_lohith@s2020.ssts.edu.sg>
2023-02-19 15:14:22 +0800

    finished realtime sharing

--- src/server/modules/realtime_sharing.py
+++ src/server/modules/realtime_sharing.py
@@ -1,31 +1,48 @@
 """Implements real-time sharing."""
-
+import aiohttp
 from aiohttp import web
-from typing import Callable
+from typing import Awaitable, Callable, Optional
 
 
 class WebSocketManager:
     """Handles WebSocket communication via a pub/sub system."""
 
     def __init__(self):
-        # Maps topics to the callbacks of subscribed user sessions
-        self._subscriptions: dict[str, set[Callable[[], str]]] = {}
-        # Maps session tokens to functions that unsubscribe
-        # the session from all of its subscribed topics
+        # Maps topics to functions that update the corresponding UI
+        self._topic_based_updaters: dict[str, set[Callable[[], Awaitable[None]]]] = {}
+        # Maps session tokens to functions that update
+        # the UI when a WebSocket message is received
+        self._message_based_updaters: dict[str, Callable[[str], Awaitable[None]]] = {}
+
+        # Maps session tokens to functions that unregister
+        # all the UI updaters associated with the session
         self._unsubscribers: dict[str, Callable[[], None]] = {}
+        # Maps session tokens to their corresponding WebSocketResponse objects
+        self._connections: dict[str, web.WebSocketResponse] = {}
 
     async def handle_connection(self, session_token: str, ws: web.WebSocketResponse):
         """
         Sends HTML updates through the WebSocket when they're
         published. Returns when the connection is closed.
         """
-        # TODO: Asynchronously loop through received messages
-        #     TODO: When a message is received, call the session's empty topic (if it has one)
+        # Save the WebSocket connection to allow other methods to access it
+        self._connections[session_token] = ws
+
+        # Asynchronously loop through received messages
+        async for msg in ws:
+            # Call the session's message-based UI updater (if it has one)
+            if session_token in self._message_based_updaters:
+                self._message_based_updaters[session_token](msg.data)
+
+        if session_token in self._unsubscribers:
+            self._unsubscribers.pop(session_token)()
+        self._connections.pop(session_token)
 
     def subscribe(
         self,
         session_token: str,
-        topic_callbacks: dict[str, Callable[[], str]],
+        topic_renderers: dict[str, Callable[[], str]],
+        receiving_renderer: Optional[Callable[[str], str]],
     ):
         """
         Subscribes the user session to the specified topics,
@@ -36,15 +53,44 @@ class WebSocketManager:
         HTML. If an empty string is used as a topic, the corresponding
         callback will be called whenever a WebSocket message is received.
         """
-        # TODO: Call the unsubscriber for this session (if there is one)
+        # Unsubscribe this session from all updates it's already subscribed to
+        if session_token in self._unsubscribers:
+            self._unsubscribers.pop(session_token)()
 
-        # TODO: Add the callbacks in topic_callbacks to self._subscriptions
+        # Maps topics to functions that update the UI for the corresponding topic
+        topic_ui_updaters = {}
 
-        def unsub_callback():
+        for topic in topic_renderers:
+
+            async def update_topic_ui():
+                """Render updated HTML and send it to the client to update their UI."""
+                ws = self._connections[session_token]
+                updated_html = topic_renderers[topic]()
+                await ws.send_str(updated_html)
+
+            # Subscribe the UI updater to the topic
+            self._topic_based_updaters[topic].add(update_topic_ui)
+            topic_ui_updaters[topic] = update_topic_ui
+
+        async def update_message_ui(msg: str):
+            """Render updated HTML and send it to the client to update their UI."""
+            if receiving_renderer:
+                ws = self._connections[session_token]
+                rendered_msg = receiving_renderer(msg)
+                await ws.send_str(rendered_msg)
+
+        def unsubscribe():
             """Unsubscribes this session from all its newly subscribed topics."""
-            # TODO: Remove each topic callback in topic_callbacks from self._subscriptions
+            # Unsubscribe each topic UI updater from their corresponding topic
+            for topic in topic_ui_updaters:
+                self._topic_based_updaters[topic].remove(topic_ui_updaters[topic])
 
-        # TODO: Add unsub_callback to self._unsubscribers
+            # Remove the message UI updater if it exists
+            if receiving_renderer is not None:
+                self._message_based_updaters.pop(session_token)
+
+        # Register the unsubscriber
+        self._unsubscribers[session_token] = unsubscribe
 
     async def publish_update(self, topics: list[str]):
         """
@@ -52,3 +98,6 @@ class WebSocketManager:
         updates their UI using the registered rendering callback.
         """
         # TODO: Call all the callbacks of the specified topics from self._subscriptions
+        for topic in topics:
+            for callback in self._topic_based_updaters[topic]:
+                callback()
